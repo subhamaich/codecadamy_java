ef137320c0cbe18fae18069ac1438086b4c15a4f
subhamaich <71261258+subhamaich@users.noreply.github.com>
2020-10-14 21:00:13 +0530

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,25 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    //We will plant a tree for you!
+    System.out.println("Hey there, I'm somebody and I'm searching nobody.");
+    System.out.println("I'm going to plant a tree today.");
+    System.out.println("    *    ");
+    System.out.println("   ***   ");
+    System.out.println("  *****  ");
+    System.out.println(" ******* ");
+    System.out.println("    *    ");
+    System.out.println("    *    ");
+    System.out.println("   ***   ");
+
+    System.out.println("Now I'm going to plant a different tree.");
+    System.out.println("  *******   ");
+    System.out.println(" *********  ");
+    System.out.println(" *********  ");
+    System.out.println(" *********  ");
+    System.out.println("  *******   ");
+    System.out.println("     *      ");
+    System.out.println("     *      ");
+    System.out.println("    ***     ");
+  }
+}
