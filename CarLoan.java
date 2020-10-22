9faf027c64d885724c85c77371b4afb01ee9e165
subhamaich <71261258+subhamaich@users.noreply.github.com>
2020-10-22 15:31:23 +0530

    Create CarLoan.java

---
+++ CarLoan.java
@@ -0,0 +1,24 @@
+//Car Loan Down Payment Calculator
+public class CarLoan {
+	public static void main(String[] args) {
+    int carLoan = 10000;
+    int loanLength = 3;
+    int interestRate = 5;
+    int downPayment = 2000;
+
+    if(loanLength <= 0 || interestRate <= 0){
+      System.out.println("Something's wrong");
+    }
+    else if(downPayment >= carLoan){
+      System.out.println("The car can be paid in full");
+    }
+    else{
+      int remainingBalance = carLoan - downPayment;
+      int months = loanLength*12;
+      int monthlyBalance = remainingBalance/months;
+      int interest = (monthlyBalance*interestRate)/100;
+      int monthlyPayment = monthlyBalance + interest;
+      System.out.println(monthlyPayment);
+    }
+	}
+}
